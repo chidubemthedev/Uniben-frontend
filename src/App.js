5e58fd04682a934f1b7367266bfd11ecb7dae50e
Trillzdboy <61975856+Trillzdboy@users.noreply.github.com>
2022-07-17 12:48:13 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import 'stream-chat-react/dist/css/index.css'
+import './App.css';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelContainer, ChannelListContainer, Auth } from './components';
+
+const cookies = new Cookies();
+
+const apiKey = 'w87tbht572rw';
+
+const authToken = cookies.get('token');
+
+const client = StreamChat.getInstance(apiKey);
+
+if (authToken) {
+  client.connectUser(
+    {
+      id: cookies.get('userId'),
+      name: cookies.get('username'),
+      fullName: cookies.get('fullName'),
+      image: cookies.get('avatarURL'),
+      hashedPassword: cookies.get('hashedPassword'),
+      phoneNumber: cookies.get('phoneNumber'),
+    },
+    authToken
+  );
+}
+
+const customStyles = {
+  '--primary-color': 'purple',
+  '--md-font': '1.2rem',
+  '--xs-m': '1.2rem',
+  '--xs-p': '1.2rem',
+};
+
+function App() {
+  const [createType, setCreateType] = useState('');
+  const [isCreating, setIsCreating] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
+  if (!authToken) {
+    return <Auth />;
+  }
+
+  return (
+    <>
+      <div className="app__wrapper">
+        <Chat client={client} customStyles={customStyles}>
+          <ChannelListContainer
+            isCreating={isCreating}
+            setIsCreating={setIsCreating}
+            setIsEditing={setIsEditing}
+            setCreateType={setCreateType}
+          />
+          <ChannelContainer
+            isCreating={isCreating}
+            setIsCreating={setIsCreating}
+            setIsEditing={setIsEditing}
+            isEditing={isEditing}
+            createType={createType}
+          />
+        </Chat>
+      </div>
+    </>
+  );
+}
+
+export default App;
