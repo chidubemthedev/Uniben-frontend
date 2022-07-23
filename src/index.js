e3f011059195e80cefffddf5b12f28ed5e545194
Trillzdboy <chukwudubem7@gmail.com>
2022-07-23 02:10:29 +0100

    added pwa optimizations

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import * as serviceWorker from './serviceWorker';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+serviceWorker.register();
